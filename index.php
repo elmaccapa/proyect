e009b34ae1f692341769f6dd01f635bfa4fb9822
juan manuel <elmaccapabayona@gmail.com>
2016-03-04 11:29:51 -0500

    primer proyecto

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>proyect</title>
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="proyec.css">
+	
+</head>
+<body background="C:\Users\Elma\Desktop\proyect\imagen\toma.jpg">
+
+<div  class="contenedor">
+<nav>
+
+
+	<ul class="menu-lista">
+
+<li class="menu-item"><a href="" class="menu-link">INICIOS</a></li>
+<li class="menu-item"><a href="" class="menu-link">HISTORIA</a></li>
+<li class="menu-item"><a href="" class="menu-link">ÉPOCA  PRECOLOMBIANA</a></li>
+<li class="menu-item"><a href="" class="menu-link">ÉPOCA  COLONIAL</a></li>
+<li class="menu-item"><a href="" class="menu-link">ÉPOCA  REPUBLICANA</a></li>
+</ul>
+<style>
+h1{
+	text-align: center;
+}
+p{
+	text-align: justify;
+}
+	
+</style>
+<div class="slider">
+<img class="img"src="imagen/loo.jpg" alt="plato tipico del perú" width="800" >
+	
+<h1><u>	HISTORIA</u></h1>
+<p> <b>La cocina peruana es considerada una de las más variadas del mundo. Gracias a la herencia pre incaica, incaica y española con 774 años de convivencia musulmana, y a la inmigración africana, francesa, chino-cantonesa, japonesa e italiana principalmente hasta el siglo XIX, reúne una gran diversidad de mezclas, junto con la criolla, en una gastronomia de cuatro continentes en tan solo un país, ofreciendo una importante variedad de platos típicos de arte culinario peruano en constante evolución, imposible de enumerarlos en su totalidad. Solo en la costa peruana, hay más de dos mil sopas diferentes y que en el país hay más de 250 postres tradicionales.</b> </p>
+ 
+<p><b>Hoy en día cuando un visitante o incluso un peruano se sientan a degustar alguno de los platos peruanos, quizá no sabe que está experimentando el resultado de una fascinante evolución de comidas y culturas. Con la llegada de los españoles se introdujo nuevas especies de animales, frutas y plantas. Esta fusión de la comida inca con la española dio a luz a la comida criolla apareciendo nuevas recetas como la Ocopa o la quinua que era considerada una comida sagrada por los incas.
+Pero la historia continua, la llegada de los esclavos africanos que cocinaban en las cocinas del Virreinato, aporto con más de un granito de arena a la evolución de nuestra cocina aportando sabor y color, es gracias a ellos que nace el delicioso anticucho de corazón. </b></p>
+ 
+<p><b>En el siglo XIX la revolución peruana se avivo. La presencia y aporte francés a la comida peruana es el resultado de la fascinación que tenían los Libertadores a todos los productos franceses. Después de la independencia, San Martin decreto la entrada libre a los extranjeros. En 1857 había un estimado de 20,000 europeos viviendo en Lima, estos incluían franceses, escoceses, ingleses, alemanes e italianos, así como también ciudadanos de la mayoría de países escandinavos y el resto de la cuenca del mediterráneo. Por otra parte en 1899, llegaron los primeros inmigrantes japoneses, dejando su huella distintiva y esencial en todas las comidas del Perú.
+Como hemos observado la cocina peruana representa una simbiosis de culturas culinarias. No solo tiene hondas raíces preincas, incas y coloniales, sino también que a lo largo de su historia ha incorporado estilos gastronómicos mulatos, europeos y orientales. </b></P>
+<div class="slider">
+<img class="img"src="imagen/trdicion.png" alt="plato tipico del perú" width="500" >
+
+<h2><u>ÉPOCA PRECOLOMBIANA</u></h2>
+
+<P><b>Los andes centrales peruanos fueron el más grande centro de domesticación de plantas del mundo antiguo, con especies nativas como el maíz, tubérculos con dos mil quinientas variedades de papa, muchas de camote, yuca o mandioca, oca, maca; gramíneas quinua, kiwicha o amaranto, cañihua; frutas como chirimoya, lúcuma, pacae, tomate, calabaza, palta, tumbo, sauco; leguminosas tales como frijoles, pallares, maní y una infinidad de hierbas aromáticas. </b></P>
+
+<P><b>Antes del arribo europeo, la geografía peruana albergaba una gran variedad de culturas, conquistadas todas por el Imperio incaico, cada una de las cuales tenía características gastronómicas particulares, aunque había algunas generalidades, de acuerdo con los cronistas de la conquista. Por ejemplo, los principales condimentos eran hierbas aromáticas, cocha yuyo (un tipo de alga fluvial), sal y, sobre todo, el ají, llamado uchu en tiempos incas y considerado hoy un elemento fundamental de la cocina peruana.</b></P>
+
+<div class="slider">
+<img class="img"src="imagen/PRE.jpg" alt="plato tipico del perú" width="500" >
+<H3><u>ÉPOCA COLONIAL</u></H3>
+<P>
+<b>Desde el inicio de la presencia española, se incorporaron nuevos usos y costumbres culinarios con el comienzo del virreinato. La fritura, el uso de los lácteos (incorporado a algunos chupes o sopas), además de la carne de res, cerdo, huevo de gallina y nuevas aves de corral; además llegaron algunos cultivos que resultarían esenciales para la nueva cocina como la cebolla y el ajo que combinados con el ají serían los principales ingredientes de muchos platos peruanos.
+
+La lima traída por los españoles y adaptado con el tiempo a la tierra peruana, se fue transformando en la variedad peruana actual denominada limón, de color verde, pequeño y ácido y que deviene en uno de los componentes básicos del cebiche. La vid (de la que se origina el pisco) y los vinos llegan también al comienzo de este período. En los primeros encuentros entre españoles y nativos, durante la conquista del imperio incaico, intercambiaron los trozos de cerdo ibérico frito con papas, camotes y maíz autóctono. </b></P>
+
+<div class="slider">
+<img class="img"src="imagen/TAM.jpg" alt="plato tipico del perú" width="500" >
+
+<H4><u>ÉPOCA REPUBLICANA</u></H4>
+<P><b>	Luego de la independencia se dieron una serie de migraciones de diversas procedencias que integraron sus propias tradiciones a la ya dinámica culinaria local. La migración de los chinos-cantoneses de mediados del siglo XIX popularizó el salteado a fuego fuerte y los sabores agridulces en las carnes además del uso de nuevas hierbas y de la salsa de soya (sillao). Pero su aporte más notorio fue el arroz. Si bien ya se consumía desde el siglo XVI, es luego de la migración china que el arroz se popularizó y se convirtió en la guarnición peruana por excelencia, en detrimento del pan.</b></P>
+
+<P><b>La forma de arroz favorita en el Perú es el arroz graneado no demasiado cocido, se hace con arroz de grano largo, sin embargo, se distanciaba de la preparación china en el uso del ajo y la sal. Otra inmigración en la segunda mitad del siglo XIX no menos influyente es la italiana, que popularizó el uso de las pastas, el pastel de acelga, los dulces y postres como el panetón, que es obligado en las navidades a lo largo del país.</b></P>
+
+</div>
+
+</nav>
+<div class="slider">
+<img class="img"src="imagen/aji.jpg" alt="plato tipico del perú" width="280" >
+<img class="img"src="imagen/CEVICHE.jpg" alt="plato tradicional del perú" width="288" >
+<img class="img"src="imagen/chiriuchu.jpg" alt="plato representativo del perú" width="250" >
+
+<embed src="MUSICA/el_condor_pasa.mp3" type=""></embed>
+
+</body>
+</html>
